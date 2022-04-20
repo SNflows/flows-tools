d290416ce0c56d7d6a2cf92340a4eb2285174aaf
Emir Karamehmetoglu <emir.k@phys.au.dk>
2022-04-20 02:32:37 +0200

    setup github CI with secrets, modifying tests.

--- flows_get_brightest/tests.py
+++ flows_get_brightest/tests.py
@@ -1,12 +1,13 @@
-import os
 import pytest
 import astropy.units as u
 import tendrils
 from .observer import get_flows_observer, Observer
 from .auth import test_connection
 from .plots import Plotter
+from .utils import api_token
 
 rot, tid, shifta, shiftd = 30, 8, 10, 10
+token = api_token()
 
 
 @pytest.mark.skip(reason="Skip until destructive overwrite is fixed")
@@ -20,40 +21,17 @@ def test_auth(monkeypatch):
 
 
 @pytest.fixture
-def api_token():
-    """Try to get the API token fron environment variable or from tendrils config.ini"""
-    token = os.environ.get('FLOWS_API_TOKEN')
-    if token is not None:
-        return token
-
-    cfg = tendrils.utils.load_config()
-    token = cfg.get('api', 'token', fallback='None')
-    if token.lower() in ['', 'None', 'bad', 'bad_token', 'badtoken']:
-        return None
-    return token
-
-
-@pytest.fixture
-def setup_tendrils(api_token):
-    tendrils.utils.set_api_token(api_token, overwrite=True)
-
-
-@pytest.fixture
-def observer(api_token):
+def observer():
     return get_flows_observer(rot, tid, shifta, shiftd)
 
 
-def test_get_brightest(capsys, observer, api_token):
-    if api_token is None:
-        pytest.xfail("No API token, cannot run tests.")
+def test_get_brightest(capsys, observer):
     observer.check_bright_stars(region=observer.regions[0])
     captured = capsys.readouterr()
     assert "Brightest star has" in captured.out
 
 
 def test_plan(observer):
-    if api_token is None:
-        pytest.xfail("No API token, cannot run tests.")
     assert observer.plan.rotation == rot * u.deg
     assert observer.plan.alpha == shifta * u.arcsec
     assert observer.plan.delta == shiftd * u.arcsec
@@ -62,15 +40,11 @@ def test_plan(observer):
 
 
 def test_observer(observer):
-    if api_token is None:
-        pytest.xfail("No API token, cannot run tests.")
     isinstance(observer, Observer)
 
 
 @pytest.mark.slow
-def test_make_finding_chart(observer, monkeypatch, api_token):
-    if api_token is None:
-        pytest.xfail("No API token, cannot run tests.")
+def test_make_finding_chart(observer, monkeypatch):
     import matplotlib.pyplot as plt
     monkeypatch.setattr(plt, 'show', lambda: None)
     plotter = Plotter(observer)
--- flows_get_brightest/utils.py
+++ flows_get_brightest/utils.py
@@ -1,8 +1,10 @@
+import os
 import argparse
 from typing import Union
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
+import tendrils
 
 # custom types
 numeric = Union[float, int, u.Quantity]
@@ -27,3 +29,23 @@ def parse():
         args.target = int(args.target)
     return args.rotate, args.target, args.shifta, args.shiftd, args.plot
 
+
+def api_token():
+    """Try to get the API token from environment variable or from tendrils config.ini"""
+    token = str(os.environ.get('FLOWS_API_TOKEN'))
+    if token is not None and token != 'None':
+        tendrils.utils.set_api_token(token=token, overwrite=True)
+        return token
+
+    cfg = tendrils.utils.load_config()
+    token = cfg.get('api', 'token', fallback='None')
+    if token.lower() in ['', 'none', 'bad', 'bad_token', 'badtoken']:
+        token = None
+    else:
+        tendrils.utils.set_api_token(token=token, overwrite=True)
+    return token
+
+
+if __name__ == '__main__':
+    token = api_token()
+    tendrils.utils.set_api_token('None')
